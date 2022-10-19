f8872c23131a82e1551b32f8ea693031727bfbfd
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 13:54:18 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 
 app.set("port", process.env.PORT || 3001);
 //Syncing all the models at once.
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   const pass = bcrypt.hashSync("HenryGrupo08", 10);
   const userCreated = await User.create({
     name: "admin",
