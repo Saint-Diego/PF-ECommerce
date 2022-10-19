4689e8905cede5c4183c455215d06e9dae45a0e2
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 15:53:06 -0500

    Update setHeader.js

--- server/src/util/middleware/setHeader.js
+++ server/src/util/middleware/setHeader.js
@@ -1,9 +1,8 @@
 const setHeader = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://don-quijote.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
 }
 
 module.exports = setHeader
