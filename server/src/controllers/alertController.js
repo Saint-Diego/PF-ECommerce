544d6e8af089fdb9a87e68d3569e5d334f388844
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 17:54:07 -0500

    Update alertController.js

--- server/src/controllers/alertController.js
+++ server/src/controllers/alertController.js
@@ -1,20 +1,35 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const HttpError = require("../errors/http-error");
-//const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
 
 const alertController = {
   sendEmail: async (req, res, next) => {
     const { emails, subject, content } = req.body;
 
     try {
+      const oAuthsClient = new google.auth.OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        REDIRECT_URI
+      );
+      oAuthsClient.setCredentials({ refresh_token: REFRESH_TOKEN });
+      const accessToken = await oAuthsClient.getAccessToken();
       const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
-            user: 'layne39@ethereal.email',
-            pass: 'Msefb1jpPkVEwmcu7X'
-        }
+          type: "OAuth2",
+          user: "sigl.system@gmail.com",
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken,
+        },
+        tls: {
+          rejectUnauthorized: true,
+        },
       });
       
       let htmlContent = `
