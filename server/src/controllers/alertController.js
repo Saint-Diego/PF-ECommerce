753b1fc4ac3c9d9b4392fd3c7cf106cdc3fc631a
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 17:09:44 -0500

    Update alertController.js

--- server/src/controllers/alertController.js
+++ server/src/controllers/alertController.js
@@ -1,34 +1,27 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const HttpError = require("../errors/http-error");
-const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
+//const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
 
 const alertController = {
   sendEmail: async (req, res, next) => {
     const { emails, subject, content } = req.body;
 
     try {
-      const oAuthsClient = new google.auth.OAuth2(
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URI
-      );
-      oAuthsClient.setCredentials({ refresh_token: REFRESH_TOKEN });
-      const accessToken = await oAuthsClient.getAccessToken();
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        //service: "gmail",
+        host: "smtp.cftpucv.cl",
+        port: 587,
+        secure: false,
         auth: {
-          type: "OAuth2",
-          user: "sigl.system@gmail.com",
-          clientId: CLIENT_ID,
-          clientSecret: CLIENT_SECRET,
-          refreshToken: REFRESH_TOKEN,
-          accessToken,
+          user: "fuentes.rodrigo@cftpucv.cl",
+          pass: "abcd1234",
         },
-        tls: {
-          rejectUnauthorized: false,
-        },
+        //tls: {
+       //   rejectUnauthorized: false,
+       // },
       });
+      
       let htmlContent = `
       <!DOCTYPE html>
       <html lang="en">
@@ -75,7 +68,7 @@ const alertController = {
       </html>
       `;
       const mailDetails = {
-        from: "'Don Quijote' <sigl.system@gmail.com>",
+        from: "'Don Quijote' <fuentes.rodrigo@cftpucv.cl>",
         to: emails,
         subject,
         html: htmlContent,
