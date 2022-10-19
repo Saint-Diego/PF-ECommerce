1c5ac7691326b598cb6e4818dc252c46ee9c2405
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 15:48:43 -0500

    Update alertController.js

--- server/src/controllers/alertController.js
+++ server/src/controllers/alertController.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
-const HttpError = require("./src/errors/http-error");
+const HttpError = require("../errors/http-error");
 const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
 
 const alertController = {
