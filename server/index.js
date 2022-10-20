b52961abf6625ac2d29d61366da56cb04ecbe575
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-19 21:02:00 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,17 +6,20 @@ const bcrypt = require("bcrypt");
 app.set("port", process.env.PORT || 3001);
 //Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-  const pass = bcrypt.hashSync("HenryGrupo08", 10);
-  const userCreated = await User.create({
-    name: "admin",
-    isActive: true,
-    lastName: "admin",
-    password: pass,
-    email: "grupo08pf@gmail.com",
-    role: "ADMIN",
-    picture: "",
-    secretWord: "admin",
-  });
+  cosnt userAdmin = await User.findOne({ where: { email: "grupo08pf@gmail.com" } });
+  if (!userAdmin) {
+    const pass = bcrypt.hashSync("HenryGrupo08", 10);
+    await User.create({
+      name: "admin",
+      isActive: true,
+      lastName: "admin",
+      password: pass,
+      email: "grupo08pf@gmail.com",
+      role: "ADMIN",
+      picture: "",
+      secretWord: "admin",
+    });
+  }
   app.listen(app.get("port"), () => {
     console.log(`Servidor escuchando en el puerto ${app.get("port")}`); // eslint-disable-line no-console
   });
